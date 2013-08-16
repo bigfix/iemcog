be8980c98ef8ba18fc101239ed4d7621d9f1737a
Karl He <khe@us.ibm.com>
2013-08-16 16:23:17 -0700

    Updated bes reader with new amazing features like addrelevance()

--- bes.py
+++ bes.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import xml.etree.ElementTree as ET
 
-class File:
+class File(object):
     def __init__(self, string):
         self.raw = string
         self.fixlets = []
@@ -11,7 +11,7 @@ class File:
             raise InputError('Not a valid BES file')
         for child in root:
             if child.tag == 'Fixlet':
-                self.fixlets.append(Fixlet(child))
+                self.fixlets.append(File.elementfactory(child))
             else:
                 print "Unrecognized Element: " + child.tag
 
@@ -20,10 +20,19 @@ class File:
         "Initialize from a .bes file"
         return cls(open(filename, 'r').read())
 
+    @classmethod
+    def elementfactory(cls, element):
+        if element.tag == 'Fixlet':
+            return Fixlet(element)
+        elif len(element) == 0 and element.text:
+            return TextElement.fromxml(element)
+        else:
+            return Element(element)
+
     def printBES(self):
         print self.raw
 
-class Element:
+class Element(object):
     def __init__(self, element):
         if not hasattr(self, 'plurals'):
             self.plurals = set()
@@ -35,15 +44,26 @@ class Element:
         for child in element:
             if child.tag in plurals:
                 if child.tag in children:
-                    children[child.tag] += [Element(child)]
+                    children[child.tag] += [File.elementfactory(child)]
                 else:
-                    children[child.tag] = [Element(child)]
+                    children[child.tag] = [File.elementfactory(child)]
             else:
-                children[child.tag] = Element(child)
+                children[child.tag] = File.elementfactory(child)
         self.children = children
         self.attributes = element.attrib
         self.text = element.text
         self.tag = element.tag
+
+    def __getattr__(self, name):
+        "Allow easier access to text-only nodes"
+        for val in self.text_accessors:
+            if name.lower() == val.lower():
+                if val in self.children:
+                    return self.children[val].text
+                else:
+                    return None
+        
+        return AttributeError
 
     def setattribute(self, name, value):
         self.attributes[name] = value
@@ -54,20 +74,61 @@ class Element:
     def haselement(self, name):
         return name in self.children
 
+    def addrelevance(self, value):
+        element = TextElement.fromstring('Relevance', value)
+        if 'Relevance' in self.children:
+            self.children['Relevance'] += [element]
+        else:
+            self.children['Relevance'] = [element]
+
 class Fixlet(Element):
     def __init__(self, element):
-        self.plurals = set(['Relevance'])
+        self.plurals = set(['Relevance', 'Action'])
         self.loadxmldata(element)
+        self.text_accessors = set([
+            'Title',
+            'Description',
+            'Category',
+            'Source',
+            'SourceID',
+            'SourceReleaseDate',
+            'SourceSeverity',
+            'CVENames',
+            'SANSID',
+            'Domain'
+            ])
 
+class TextElement(Element):
+    def __init__(self, tag, text, attributes):
+        self.tag = tag
+        self.text = text
+        self.attributes = attributes
 
+    @classmethod
+    def fromxml(cls, element):
+        return cls(element.tag, element.text, element.attrib)
 
+    @classmethod
+    def fromstring(cls, tag, text, attrs=None):
+        return cls(tag, text, attrs)
+
+    def settext(self, text):
+        self.text = text
+
+    def gettext(self):
+        return self.text
+
+# Test code
 besfile = File.fromfilename('test/test.bes')
-# besfile.printBES()
 for fixlet in besfile.fixlets:
     print fixlet.tag
-    print fixlet.children['Title'].text
-    print fixlet.children['DefaultAction'].hasattribute('id')
-    print fixlet.haselement('Relevance')
+    print fixlet.title
+    print fixlet.Description
+
+    print ''
+    print '== Relevance: =='
+    fixlet.addrelevance('true')
     for relevance in fixlet.children['Relevance']:
-        print relevance.text[0:20]
+        print relevance.text
+
 
