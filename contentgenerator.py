0c62b524951212ea026eeeebb2751ce178b30a35
isnlohbommu <aisko3@gmail.com>
2013-08-16 15:15:43 -0700

    contentgenerator in python
    Can create fixlet now

---
+++ contentgenerator.py
@@ -0,0 +1,65 @@
+from xml.etree import ElementTree as ET
+
+fixlet = """
+<BES>
+	<Fixlet>
+	</Fixlet>
+</BES>
+"""
+
+fixletaction = """
+<Action>
+    <Description>
+        <PreLink>Click </PreLink>
+        <Link>here</Link>
+        <PostLink></PostLink>
+    </Description>
+    <ActionScript MIMEType="application/x-Fixlet-Windows-Shell"></ActionScript>
+</Action>
+"""
+
+def createFixlet(filename, title, description, relevance, actions, parameters, MIMEfields):
+    t = ET.ElementTree(ET.fromstring(fixlet))
+    f = t.getroot()[0]
+    
+    node = ET.Element("Title")
+    node.text = title
+    f.append(node)
+    
+    node = ET.Element("Description")
+    lines = ""
+    for line in description:
+        lines += line + "\n"
+    node.text = lines
+    f.append(node)
+
+    for expression in relevance:
+        node = ET.Element("Relevance")
+        node.text = expression
+        f.append(node)
+
+    #parameters
+    for (key, value) in parameters.items():
+        node = ET.Element(key)
+        node.text = value
+        f.append(node)
+
+    for MIME in MIMEfields:
+        f.append(MIME)
+        
+    for action in actions:
+        f.append(action)
+    
+    t.write(filename + ".bes")
+    
+def createAction(ID, postLink, action):
+    a = ET.fromstring(fixletaction)
+    a.set('ID', ID)
+    node = a.find("Description")
+    node.find("PostLink").text = postLink
+    node = a.find("ActionScript")
+    lines = ""
+    for line in action:
+        lines += line + "\n"
+    node.text = lines
+    return a
---
+++ contentgeneratorexample.py
@@ -0,0 +1,14 @@
+import contentgenerator as cg
+id = "Action1"
+postlink = " to enable incoming traffic on the port reserved for BES."
+action = ['// enable the WSH','download http://www.symantec.com/avcenter/noscript.exe','continue if {(size of it = 127432 and sha1 of it = "c19722c97b73210065ec58fd43cbf4b0c84dd3e5") of file "noscript.exe" of folder "__download"}','wait __download/noscript.exe /silent /on','','// Change the firewall settings','run "{pathname of client folder of site "BESSupport"}\\RunQuiet.exe" "{pathname of system folder}\\cscript.exe" "{pathname of client folder of site "BESSupport"}\\icfconfirm.vbs"','','wait "{pathname of system folder}\\cscript.exe" "{pathname of client folder of site "BESSupport"}\\besport.js" 127.0.0.1 "{value "ListenPort" of key "HKEY_LOCAL_MACHINE\\SOFTWARE\\BigFix\\EnterpriseClient\\GlobalOptions" of registry}"']
+
+
+title = "Internet Connection Firewall is Blocking BES Traffic - BES Client (WSH disabled)"
+description = ["The listed computers have Windows Internet Connection Firewall (ICF) enabled. The firewall is currently configured to block inbound UDP traffic on the port used by BES (BES uses port 52311 by default).", "The BES Server and BES Relays send UDP packets to the BES Clients to notify them that there is new information available such as new Fixlet messages, actions, and computer refreshes. BES Clients on relevant computers will not receive UDP notification packets and therefore will not see new actions or new Fixlet messages until they gather the new actionsite, which is by default, once a day. After configuring Windows Firewall to allow inbound UDP traffic on the BES Listen Port, BES Clients will resume normal communication with the BES Server and BES Relays."]
+relevance = ['(if exists property "in proxy agent context" then ( not in proxy agent context ) else true )', 'version of client < "8.0"']
+actions = [cg.createAction(id, postlink, action)]
+parameters = {'Category': 'Support', 'Source': 'BigFix'}
+
+
+cg.createFixlet("Fixlet Name", title, description, relevance, actions, parameters, [])
