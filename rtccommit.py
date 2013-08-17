04238b35e611b6dde7a2c264758f3f5632d353f6
EvanZ <zevan11@gmail.com>
2013-08-16 18:10:45 -0700

    Ability to commit via changeset ID or workspace name. Also more error checking and outputs.

--- rtccommit.py
+++ rtccommit.py
@@ -8,7 +8,7 @@ import os
 # (3) Update the checked in changeset (mainly by adding comments)
 # (4) Deliver the changes
 # (5) Logout
-def rtc_commit(username, password, dir, comment='Committing changes', repoURL='https://rtp-rtc17.tivlab.raleigh.ibm.com:9443/jazz'):
+def rtc_commit(username, password, dir, comment='Committing changes', repoURL='https://rtp-rtc17.tivlab.raleigh.ibm.com:9443/jazz', changesetID="", workspace="", scmfile=""):
     if not username:
         print "Please enter a username."
         return 0
@@ -23,17 +23,19 @@ def rtc_commit(username, password, dir, comment='Committing changes', repoURL='h
         return 0
     
     # Find the RTC command line executable.
-    try:
-        programFilesDir = os.environ['PROGRAMFILES(X86)']
-    except:
+    if not scmfile:
         try:
-            programFilesDir = os.environ['PROGRAMFILES']
+            programFilesDir = os.environ['PROGRAMFILES(X86)']
         except:
-            programFilesDir = "C:\Program Files"
-    scmfile = programFilesDir + '/IBM/TeamConcert/scmtools/eclipse/scm.exe'
+            try:
+                programFilesDir = os.environ['PROGRAMFILES']
+            except:
+                programFilesDir = "C:\Program Files"
+        scmfile = programFilesDir + '/IBM/TeamConcert/scmtools/eclipse/scm.exe'
     
     if os.path.isfile(scmfile):
         # Login
+        print "Logging into RTC."
         login = subprocess.Popen([scmfile, 'login', '-r', repoURL, '-u', username, '-P', password], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         loginout, loginerr = login.communicate()
         if loginerr:
@@ -42,53 +44,63 @@ def rtc_commit(username, password, dir, comment='Committing changes', repoURL='h
         elif loginout:
             print loginout
 
-        # Check in the changes
-        checkin = subprocess.Popen([scmfile, 'checkin', '-u', username, '-P', password, dir], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        checkinout, checkinerr = checkin.communicate()
-        if checkinerr:
-            print "Error checking in directory '" + dir + "': " + checkinerr
-            rtc_logout(scmfile, repoURL)
-            return 0
-        # If successfully checked in items, update the changelist by adding comment
-        elif checkinout:
-            print checkinout
+        if not changesetID:
+            # Check in the changes
+            print "Checking in any changes."
+            checkin = subprocess.Popen([scmfile, 'checkin', '-u', username, '-P', password, dir], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            checkinout, checkinerr = checkin.communicate()
+            if checkinerr:
+                print "Error checking in directory '" + dir + "': " + checkinerr
+                rtc_logout(scmfile, repoURL)
+                return 0
+            # If successfully checked in items, update the changelist by adding comment
+            elif checkinout:
+                print checkinout
 
-            searchObj = re.search(r'\n\s+Change sets:\s*\((\d+)\)', checkinout, re.DOTALL)
-            if searchObj:
-                try:
-                    changesetID = searchObj.group(1)
-                except:
+                searchObj = re.search(r'\n\s+Change sets:\s*\((\d+)\)', checkinout, re.DOTALL)
+                if searchObj:
+                    try:
+                        changesetID = searchObj.group(1)
+                    except:
+                        print "Can't grab changeset ID from previous output. This is bad since items have been checked in."
+                        rtc_logout(scmfile, repoURL)
+                        return 0
+                else:
                     print "Can't grab changeset ID from previous output. This is bad since items have been checked in."
                     rtc_logout(scmfile, repoURL)
                     return 0
+                    
+                print "Adding a comment to changeset#" + changesetID + "."
+                changeset = subprocess.Popen([scmfile, 'changeset', 'comment', '-u', username, '-P', password, changesetID, comment], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                changesetout, changeseterr = changeset.communicate()
+                if changeseterr:
+                    print "Error writing comment to changeset#" + changesetID +": " + changeseterr
+                    rtc_logout(scmfile, repoURL)
+                    return 0
+                elif changesetout:
+                    print changesetout
+            # If nothing was checked in, then we should just move directly to "deliver/commit"
             else:
-                print "Can't grab changeset ID from previous output. This is bad since items have been checked in."
-                rtc_logout(scmfile, repoURL)
-                return 0
-                
-            changeset = subprocess.Popen([scmfile, 'changeset', 'comment', '-u', username, '-P', password, changesetID, comment], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            changesetout, changeseterr = changeset.communicate()
-            if changeseterr:
-                print "Error writing comment to changeset#" + changesetID +": " + changeseterr
-                rtc_logout(scmfile, repoURL)
-                return 0
-            elif changesetout:
-                print changesetout
-        # If nothing was checked in, then we should just move directly to "deliver/commit"
+                print "Nothing was checked in. Perhaps a previous changeset was already checked in. Will try to deliver any outstanding changesets."
         else:
-            print "Nothing was checked in. Perhaps a previous changeset was already checked in. Will try to deliver any outstanding changesets."
+            # Changeset ID provided, so will do target changeset delivery
+            print "Changeset ID provided. Will just deliver/commit that particular changeset."
         
         # Deliver/commit the changes
-        deliver = subprocess.Popen([scmfile, 'deliver', '-u', username, '-P', password, '-d', dir], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if changesetID:
+            print "Delivering/committing any changes via changeset ID " + changesetID + "."
+            deliver = subprocess.Popen([scmfile, 'deliver', '-u', username, '-P', password, '-d', dir, '-c', changesetID], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        elif workspace:
+            print "Delivering/committing any changes via workspace name " + workspace + "."
+            deliver = subprocess.Popen([scmfile, 'deliver', '-u', username, '-P', password, '-d', dir, '-s', workspace], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        else:
+            print "Delivering/committing any changes."
+            deliver = subprocess.Popen([scmfile, 'deliver', '-u', username, '-P', password, '-d', dir], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         deliverout, delivererr = deliver.communicate()
+        if deliverout:
+            print deliverout
         if delivererr:
             print "Error delivering (committing) changes: " + delivererr
-            rtc_logout(scmfile, repoURL)
-            return 0
-        elif deliverout:
-            print deliverout
-        else:
-            print "Nothing was delivered/committed."
 
         # Log out
         rtc_logout(scmfile, repoURL)
@@ -109,6 +121,7 @@ def rtc_logout(scmfile, repoURL='https://rtp-rtc17.tivlab.raleigh.ibm.com:9443/j
         scmfile = programFilesDir + '/IBM/TeamConcert/scmtools/eclipse/scm.exe'
         
     if os.path.isfile(scmfile):
+        print "Logging out of RTC."
         logout = subprocess.Popen([scmfile, 'logout', '-r', repoURL], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         logoutout, logouterr = logout.communicate()
         if logouterr:
